a886594ca97f815a2415253cb7cf195b0a8b9419
JarredAn <jarred_ndrs@yahoo.com>
2025-12-17 13:05:09 -0600

    Update project script

---
+++ Jarand5122_SDC205L_Project.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+print ("Jarand5122's Excel Spreadsheet Automation Menu")
+print ("Choose a number from the following options:")
+print ("1. Input Data")
+print ("2. View Current Data")
+print ("3. Generate Report")
+
+user_choice = input()
+
+print (f"You selected option {user_choice} at {datetime.now()}")
