de866720055aad9fcd04cfe671a8bd3b9a8fd6ab
JarredAn <jarred_ndrs@yahoo.com>
2026-01-04 16:05:48 -0600

    2.4

--- Jarand5122_SDC205L_Project.py
+++ Jarand5122_SDC205L_Project.py
@@ -7,5 +7,7 @@ print ("2. View Current Data")
 print ("3. Generate Report")
 
 user_choice = input()
-
-print (f"You selected option {user_choice} at {datetime.now()}")
+if user_choice not in ['1', '2', '3']:
+    print ("Invalid option selected. Please choose 1, 2, or 3.")
+else:
+    print (f"You selected option {user_choice} at {datetime.now()}")
