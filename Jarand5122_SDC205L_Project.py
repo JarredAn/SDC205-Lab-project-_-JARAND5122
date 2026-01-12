6dd742fe24ec267046dba9fa4f77dc4a3646a05f
JarredAn <jarred_ndrs@yahoo.com>
2026-01-11 22:28:58 -0600

    3.4 project

--- Jarand5122_SDC205L_Project.py
+++ Jarand5122_SDC205L_Project.py
@@ -1,5 +1,24 @@
 from datetime import datetime
 
+# Function to convert temperature from Fahrenheit to Celsius
+def convertData(temp_input):
+    converted_value = (temp_input - 32) * 5.0/9.0
+    return converted_value
+
+# Function: getInput prompts user for entries  and prints results
+def getInput():
+    num_entries = int(input("How many entries are you entering? "))
+    
+    for i in range(num_entries):
+        date_input = input("Enter the date: ")
+        print()
+        temp_input= float(input("Enter the temperature in farenheit): "))
+        
+        converted_value = convertData(temp_input)
+        
+        print(f"The following was saved at {datetime.now()} : ")
+        print(f"{date_input}, {temp_input}, {converted_value}")
+
 print ("Jarand5122's Excel Spreadsheet Automation Menu")
 print ("Choose a number from the following options:")
 print ("1. Input Data")
@@ -11,3 +30,8 @@ if user_choice not in ['1', '2', '3']:
     print ("Invalid option selected. Please choose 1, 2, or 3.")
 else:
     print (f"You selected option {user_choice} at {datetime.now()}")
+    
+if user_choice == '1':
+    getInput()
+else:
+    print("Error: The chosen functionality is not implemented yet.")
